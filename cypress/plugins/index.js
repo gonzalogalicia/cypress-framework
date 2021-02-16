ab3a07b710610d1c27ceaf57fa1a55138ab19900
Gonzalo Galicia <gonzalo.galicia@outlook.com>
2021-02-16 15:05:48 -0800

    Updating dependencies + cleanup

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,13 +16,9 @@
  * @type {Cypress.PluginConfig}
  */
 
-const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
-
 module.exports = (on, config) => {
 	// `on` is used to hook into various events Cypress emits
 	// `config` is the resolved Cypress config
-	addMatchImageSnapshotPlugin(on, config)
 }
 
-
-require('@applitools/eyes-cypress')(module);
+require('@applitools/eyes-cypress')(module)
